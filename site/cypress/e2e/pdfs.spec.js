ae811b9e8e5a71752cdfd664d2a615937e90a79b
IDzyre <ReaperNSGaming@gmail.com>
2023-07-07 22:12:05 -0800

    refactor finished

--- site/cypress/e2e/pdfs.spec.js
+++ site/cypress/e2e/pdfs.spec.js
@@ -1,85 +1,103 @@
-Cypress.Commands.add('switch_settings', (gradeable_id) => {
+function switch_settings(gradeable_id) {
     cy.visit(['sample', 'gradeable', gradeable_id, 'update']);
     cy.get('#page_3_nav').click();
-    // Save the previous settings to revert after. 
-    cy.get('#minimum_grading_group option:selected').invoke('text').as(gradeable_id.concat('_selected'));
-    cy.get('input[name="grader_assignment_method"]:checked').as(gradeable_id.concat('_checked'));
-    
-    cy.get('[data-testid="minimum_grading_group"]').select('Limited Access Grader');
     cy.get('input[data-testname="grader_assignment_method"][value="1"]').check();
-});
+}
 
-Cypress.Commands.add('revert_settings', (gradeable_id) => {
+function revert_settings(gradeable_id, setting) {
     cy.visit(['sample', 'gradeable', gradeable_id, 'update']);
     cy.get('#page_3_nav').click();
-    cy.get('@'.concat(gradeable_id, '_selected')).then(setting => {
-        cy.get('#minimum_grading_group').select(setting);
-        cy.get('#minimum_grading_group option:selected').should('have.text', setting);
-    });
-    cy.get('@'.concat(gradeable_id, '_checked')).check();
-});
+    cy.get(`input[data-testname="grader_assignment_method"][id=${setting}`).check();
+}
 
-Cypress.Commands.add('pdf_access', (user_id, gradeable_id) => {
-    cy.visit('/');
-    cy.login(user_id);
-
+function check_pdf_access(gradeable_id) {
     cy.visit(['sample', 'gradeable', gradeable_id, 'grading', 'details']);
     cy.get('#agree-button').click({ force: true });
     cy.get('[data-testid="details-table"]').should('be.visible');
-    if (user_id !== 'grader') {
-            cy.get('[data-testid="view-sections"').then(($button) =>{
-                if($button.text().includes('View All')){
-                    $button.click();
-                }
-            });
-        
-    }
-    // This gets a gradeable that has been graded already, so there are submissions available. 
-    cy.get('[data-testid="grade-table"]').contains('/ 12').click({ force: true });
-    // This is because some of the gradeables have the submission tab open
-    cy.get('[data-testid="show-submission"]').then($element => {
-        console.log($element.className);
-        if($element.hasClass('active')){
-            // do nothing
-        } else {
-            $element.click();
+    cy.get('[data-testid="view-sections"]').then(($button) => {
+        if ($button.text().includes('View All')) {
+            $button.click();
         }
     });
+    // This gets a gradeable that has been graded already, so there are submissions available.
+    select_gradeable();
+}
 
+function no_pdf_access(gradeable_id) {
+    cy.visit(['sample', 'gradeable', gradeable_id, 'grading', 'details']);
+    cy.get('[data-testid="banner-message"]').should('be.visible').and('contain.text', 'You do not have permission');
+}
+
+function select_gradeable() {
+    // This gets a gradeable that has been graded already, so there are submissions available.
+    cy.get('[data-testid="grade-table"]').contains('/ 12').click({ force: true });
+    cy.get('[data-testid="show-autograding"]').click();
+    cy.get('[data-testid="show-submission"]').click();
     cy.get('[data-testid="folders"]').contains('submissions').click();
     cy.get('#div_viewer_sd1').contains('words_').click();
     cy.get('#pageContainer1').should('be.visible');
-    cy.logout();
 }
-);
 
-const types = ['grading_homework_pdf', 'grading_homework_team_pdf', 'grading_pdf_peer_homework', 'grading_pdf_peer_team_homework'];
+function minimum_pdf_access(gradeable_id) {
+    cy.visit(['sample', 'gradeable', gradeable_id, 'grading', 'details']);
+    cy.get('#agree-button').click({ force: true });
+    cy.get('[data-testid="details-table"]').should('be.visible');
+    select_gradeable();
+}
+const types = ['grading_homework_team_pdf', 'grading_homework_pdf', 'grading_pdf_peer_homework', 'grading_pdf_peer_team_homework'];
 
 describe('Test cases for PDFs access', () => {
     before(() => {
-        // cy.visit('/');
-        // cy.login('instructor');
-        // types.forEach((gradeable_id) => {
-        //     cy.switch_settings(gradeable_id);
-        // });
-        // cy.logout();
+        cy.visit('/');
+        cy.login('instructor');
+        // Change the settings that aren't already
+        switch_settings('grading_homework_team_pdf');
+        switch_settings('grading_pdf_peer_homework');
+        switch_settings('grading_pdf_peer_team_homework');
+        cy.logout();
+    });
+
+    beforeEach(() => {
+        cy.visit('/');
     });
 
     after(() => {
         cy.visit('/');
         cy.login('instructor');
-        types.forEach((gradeable_id) => {
-            cy.revert_settings(gradeable_id);
-        });
+        revert_settings('grading_homework_team_pdf', 'rotating_section');
+        revert_settings('grading_pdf_peer_homework', 'all_access');
+        revert_settings('grading_pdf_peer_team_homework', 'all_access');
         cy.logout();
     });
 
-    ['grader', 'ta', 'instructor'].forEach((user) => {
-        it(`${user} should have access to pdfs`, () => {
-            types.forEach((gradeable_id) => {
-                cy.pdf_access(user, gradeable_id);
-            });
-            cy.login('student');
+
+    it('ta should have access to pdfs', () => {
+        cy.login('ta');
+        types.forEach((gradeable_id) => {
+            check_pdf_access(gradeable_id);
         });
     });
+
+    it('instructor should have access to pdfs', () => {
+        cy.login('instructor');
+        types.forEach((gradeable_id) => {
+            check_pdf_access(gradeable_id);
+        });
+    });
+
+    it('student should have access to some pdfs', () => {
+        cy.login('student');
+        minimum_pdf_access('grading_pdf_peer_homework');
+        minimum_pdf_access('grading_pdf_peer_team_homework');
+        no_pdf_access('grading_homework_team_pdf');
+        no_pdf_access('grading_homework_pdf');
+    });
+
+    it('grader should have access to some pdfs', () => {
+        cy.login('grader');
+        types.forEach((gradeable_id) => {
+            minimum_pdf_access(gradeable_id);
+        });
+    });
+
 });
